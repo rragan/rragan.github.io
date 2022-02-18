f121c0219158b6806fcddd5f1ac98072b330c616
Richard Ragan <rragan@gmail.com>
2022-02-18 13:00:27 -0800

    Create resizer.js

---
+++ resizer.js
@@ -0,0 +1,10 @@
+var password = 'e%.x9GG('
+var cacheLat = 'MDRp1ZWJ7sM='
+var cacheLng = '1eh3+EoEIV/AbRP0'
+var trailheadLat = 'g5joqL/L4+5NqVIi'
+var trailheadLng = '8LkxJhl6/ARRTVX7'
+
+//cacheLat = Tea.encrypt("37.3822", password);
+//cacheLng = Tea.encrypt("-122.06883", password);
+trailheadLat = cacheLat;
+trailheadLng = cacheLng;
